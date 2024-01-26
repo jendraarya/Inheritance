8e03905e3aeb4de2133f5be01780000da64bb62a
jendraarya <157786921+jendraarya@users.noreply.github.com>
2024-01-26 14:35:19 +0700

    Add files via upload
    Terimakasih sudah menilai saya PakSonHaji

---
+++ inheritance/dan/override/BangunDatar.java
@@ -0,0 +1,15 @@
+
+package inheritance.dan.override;
+
+public class BangunDatar {
+    
+    float luas(){
+        System.out.println("Menghitung luas bangun datar");
+        return 0;
+    }
+    
+    float keliling(){
+        System.out.println("Menghitung keliling bangun datar");
+        return 0;
+    }
+}
---
+++ inheritance/dan/override/Persegi.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inheritance.dan.override;
+
+/**
+ *
+ * @author SMK TELKOM 30
+ */
+class Persegi {
+
+    int sisi;
+    
+}
